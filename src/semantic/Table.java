d372af9256c6ec1e9b754b4e9ce4b44a1f540251
Miguel <miguelangelo_teixeira@hotmail.com>
2017-05-27 17:27:15 +0100

    Added functionLookup

--- src/semantic/Table.java
+++ src/semantic/Table.java
@@ -43,5 +43,18 @@ public class Table {
 			return null;
 		
 	}
+    
+   public Table lookupFunction(String name){
+	   int nodeNum;
+	   while(parent != null){
+		   parent.lookupFunction(name);
+	   }
+	   for(Table t : parent.children){
+		  nodeNum = t.getSymbol(0).getType().equals("function name") ? 0 : 1;
+		  if(t.getSymbol(nodeNum).getName().equals(name))
+			  return t;
+	   }
+	   return null;
+   }
 	
 }
